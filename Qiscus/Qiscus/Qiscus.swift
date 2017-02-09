a0ed76c34095f58c45c71ecb86ecf80245c5528f
Ahmad Athaullah <ahmad.athaullah@gmail.com>
2017-02-09 10:34:53 +0700

    turn off debug print

--- Qiscus/Qiscus/Qiscus.swift
+++ Qiscus/Qiscus/Qiscus.swift
@@ -17,7 +17,7 @@ import UserNotifications
 open class Qiscus: NSObject, MQTTSessionDelegate, PKPushRegistryDelegate, UNUserNotificationCenterDelegate {
 
     open static let sharedInstance = Qiscus()
-    static let showDebugPrint = true
+    static let showDebugPrint = false
     
     open var config = QiscusConfig.sharedInstance
     open var commentService = QiscusCommentClient.sharedInstance
