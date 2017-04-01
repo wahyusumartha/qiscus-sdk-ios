c1930e81988b2da510f658fdd3d4645aced84009
Ahmad Athaullah <ahmad.athaullah@gmail.com>
2017-04-02 06:44:33 +0700

    - add new layer for database object [iPhone 5 app stopped bug]

---
+++ Qiscus/Qiscus/QiscusFileDB.swift
@@ -0,0 +1,13 @@
+//
+//  QiscusFileDB.swift
+//  Example
+//
+//  Created by Ahmad Athaullah on 4/1/17.
+//  Copyright © 2017 Ahmad Athaullah. All rights reserved.
+//
+
+import UIKit
+
+class QiscusFileDB: Object {
+
+}
