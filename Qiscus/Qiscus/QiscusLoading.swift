bca8e3943c03b18adfff192c3c74ed6e9f9ad1c9
Ahmad Athaullah <ahmad.athaullah@gmail.com>
2017-05-05 16:37:34 +0700

    hot fix - crash on showing loading

--- Qiscus/Qiscus/QiscusLoading.swift
+++ Qiscus/Qiscus/QiscusLoading.swift
@@ -12,37 +12,37 @@ public extension UIViewController{
     @objc public func showQiscusLoading(withText text:String? = nil, andProgress progress:Float = 0, isBlocking:Bool = false){
         Qiscus.uiThread.async {
             let loadingView = QLoadingViewController.sharedInstance
-            if loadingView.isPresence{
+            if !loadingView.isPresence{
                 self.dismissQiscusLoading()
-            }
-            loadingView.modalTransitionStyle = .crossDissolve
-            loadingView.modalPresentationStyle = .overCurrentContext
-            loadingView.view.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.7)
-            
-            if text == nil {
-                loadingView.loadingLabel.isHidden = true
-                loadingView.loadingLabel.text = ""
-            }else{
-                loadingView.loadingLabel.isHidden = false
-                loadingView.loadingLabel.text = text
-            }
-            loadingView.isBlocking = isBlocking
-            if progress == 0{
-                loadingView.percentageLabel.text = ""
-                loadingView.percentageLabel.isHidden = true
-            }else{
-                let percentage:Int = Int(progress * 100)
-                loadingView.percentageLabel.text = "\(percentage)%"
-                loadingView.percentageLabel.isHidden = false
-            }
-            
-            self.present(loadingView, animated: false, completion: {
-                if QLoadingViewController.sharedInstance.dismissImmediately{
-                    self.dismissQiscusLoading()
+                loadingView.modalTransitionStyle = .crossDissolve
+                loadingView.modalPresentationStyle = .overCurrentContext
+                loadingView.view.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.7)
+                
+                if text == nil {
+                    loadingView.loadingLabel.isHidden = true
+                    loadingView.loadingLabel.text = ""
                 }else{
-                    loadingView.isPresence = true
+                    loadingView.loadingLabel.isHidden = false
+                    loadingView.loadingLabel.text = text
                 }
-            })
+                loadingView.isBlocking = isBlocking
+                if progress == 0{
+                    loadingView.percentageLabel.text = ""
+                    loadingView.percentageLabel.isHidden = true
+                }else{
+                    let percentage:Int = Int(progress * 100)
+                    loadingView.percentageLabel.text = "\(percentage)%"
+                    loadingView.percentageLabel.isHidden = false
+                }
+                
+                self.present(loadingView, animated: false, completion: {
+                    if QLoadingViewController.sharedInstance.dismissImmediately{
+                        self.dismissQiscusLoading()
+                    }else{
+                        loadingView.isPresence = true
+                    }
+                })
+            }
         }
     }
     public func dismissQiscusLoading(){
