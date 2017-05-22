59a241ab50a496ca13053faf41f73b5476b7524e
Ahmad Athaullah <ahmad.athaullah@gmail.com>
2017-05-22 09:21:28 +0700

    add cell type : post back

---
+++ Qiscus/Qiscus/QCellPostbackLeft.swift
@@ -0,0 +1,18 @@
+//
+//  QCellPostbackLeft.swift
+//  Example
+//
+//  Created by Ahmad Athaullah on 5/18/17.
+//  Copyright © 2017 Ahmad Athaullah. All rights reserved.
+//
+
+import UIKit
+
+class QCellPostbackLeft: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
