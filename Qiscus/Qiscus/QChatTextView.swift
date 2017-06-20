e0e2391bab7942320db87b504f7e2a4afd8939b9
Ahmad Athaullah <ahmad.athaullah@gmail.com>
2017-06-21 06:45:07 +0700

    fix tap link problem

--- Qiscus/Qiscus/QChatTextView.swift
+++ Qiscus/Qiscus/QChatTextView.swift
@@ -13,10 +13,12 @@ class QChatTextView: UITextView {
     override func becomeFirstResponder() -> Bool {
         return false
     }
-
+    
     override func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
         if !isEditable {
             if let gesture = gestureRecognizer as? UILongPressGestureRecognizer, gesture.minimumPressDuration == 0.5 {
+                print("gestureRecognizer minimumPD: \(gesture.minimumPressDuration)")
+                
                 return false
             }
         }
