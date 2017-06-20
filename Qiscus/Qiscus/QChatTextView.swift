a71e0b8febcec782cf8d4413467ad528ec89868a
Ahmad Athaullah <ahmad.athaullah@gmail.com>
2017-06-21 05:13:18 +0700

    add new class to handle UITextView

---
+++ Qiscus/Qiscus/QChatTextView.swift
@@ -0,0 +1,21 @@
+//
+//  QChatTextView.swift
+//  Example
+//
+//  Created by Ahmad Athaullah on 6/20/17.
+//  Copyright © 2017 Ahmad Athaullah. All rights reserved.
+//
+
+import UIKit
+
+class QChatTextView: UITextView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
