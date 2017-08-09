0169a5334c2c76f97ac7533e34a11df5dbc8cf4d
Ahmad Athaullah <ahmad.athaullah@gmail.com>
2017-08-09 10:33:00 +0700

    change comment.type to string

--- Qiscus/Qiscus/QiscusConfig.swift
+++ Qiscus/Qiscus/QiscusConfig.swift
@@ -13,7 +13,7 @@ open class QiscusConfig: NSObject {
     static let sharedInstance = QiscusConfig()
     
     open var commentPerLoad:Int = 10
-    open var dbSchemaVersion:UInt64 = 39
+    open var dbSchemaVersion:UInt64 = 40
     
     open var UPLOAD_URL = ""
     
