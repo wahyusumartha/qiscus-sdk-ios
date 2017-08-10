b2bd6c975abdd9ea999c2b229a8b06d5d68631e3
Ahmad Athaullah <ahmad.athaullah@gmail.com>
2017-08-10 09:35:01 +0700

    add assets for contact cell, remove unused asset, create cell for contactShare

---
+++ Qiscus/Qiscus/QCellContactRight.swift
@@ -0,0 +1,18 @@
+//
+//  QCellContactRight.swift
+//  Example
+//
+//  Created by Ahmad Athaullah on 8/10/17.
+//  Copyright © 2017 Ahmad Athaullah. All rights reserved.
+//
+
+import UIKit
+
+class QCellContactRight: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
