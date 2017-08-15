fe5119ce24b64108963f567b49fb1e24326fde23
Ahmad Athaullah <ahmad.athaullah@gmail.com>
2017-08-15 15:13:50 +0700

    increase dbSchema version

--- Qiscus/Qiscus/Configuration/QiscusConfig.swift
+++ Qiscus/Qiscus/Configuration/QiscusConfig.swift
@@ -13,7 +13,7 @@ open class QiscusConfig: NSObject {
     static let sharedInstance = QiscusConfig()
     
     open var commentPerLoad:Int = 10
-    open var dbSchemaVersion:UInt64 = 42
+    open var dbSchemaVersion:UInt64 = 43
     
     open var UPLOAD_URL = ""
     
