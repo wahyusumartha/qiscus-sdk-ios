eec0a4667053922b87cc471573447349543f06ee
Ahmad Athaullah <ahmad.athaullah@gmail.com>
2017-08-23 10:06:26 +0700

    add file needed to render contact share message

---
+++ Qiscus/Qiscus/View/QChatCell/QCellContactLeft.swift
@@ -0,0 +1,18 @@
+//
+//  QCellContactLeft.swift
+//  Example
+//
+//  Created by Ahmad Athaullah on 8/23/17.
+//  Copyright © 2017 Ahmad Athaullah. All rights reserved.
+//
+
+import UIKit
+
+class QCellContactLeft: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
