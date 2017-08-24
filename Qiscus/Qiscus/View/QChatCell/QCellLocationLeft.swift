1eded609e62ba51a1d8f957a84c5684660ae8c39
Ahmad Athaullah <ahmad.athaullah@gmail.com>
2017-08-24 12:47:16 +0700

    add file needed for rendering location message

---
+++ Qiscus/Qiscus/View/QChatCell/QCellLocationLeft.swift
@@ -0,0 +1,18 @@
+//
+//  QCellLocationLeft.swift
+//  Example
+//
+//  Created by Ahmad Athaullah on 8/24/17.
+//  Copyright © 2017 Ahmad Athaullah. All rights reserved.
+//
+
+import UIKit
+
+class QCellLocationLeft: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ Qiscus/Qiscus/View/QChatCell/QCellLocationRight.swift
@@ -0,0 +1,18 @@
+//
+//  QCellLocationRight.swift
+//  Example
+//
+//  Created by Ahmad Athaullah on 8/24/17.
+//  Copyright © 2017 Ahmad Athaullah. All rights reserved.
+//
+
+import UIKit
+
+class QCellLocationRight: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
