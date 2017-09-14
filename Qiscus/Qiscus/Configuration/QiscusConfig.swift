0013a33ed4451b9a8d2a73dba04464a9a1faef79
Ahmad Athaullah <ahmad.athaullah@gmail.com>
2017-09-14 10:37:28 +0700

    update DB version for property change

--- Qiscus/Qiscus/Configuration/QiscusConfig.swift
+++ Qiscus/Qiscus/Configuration/QiscusConfig.swift
@@ -13,7 +13,7 @@ open class QiscusConfig: NSObject {
     static let sharedInstance = QiscusConfig()
     
     open var commentPerLoad:Int = 10
-    open var dbSchemaVersion:UInt64 = 51
+    open var dbSchemaVersion:UInt64 = 52
     
     open var UPLOAD_URL = ""
     
