3ef2ad1b2133e93c0192906410e52b9613b185db
Ahmad Athaullah <ahmad.athaullah@gmail.com>
2017-09-28 13:28:18 +0700

    try to prevent wrong message sequence

--- Qiscus/Qiscus/Service/QRoomService.swift
+++ Qiscus/Qiscus/Service/QRoomService.swift
@@ -41,7 +41,7 @@ public class QRoomService:NSObject{
                         if commentId <= QiscusMe.sharedInstance.lastCommentId {
                             room.saveOldComment(fromJSON: json)
                         }else{
-                            room.saveNewComment(fromJSON: json)
+                            //room.saveNewComment(fromJSON: json)
                             QiscusBackgroundThread.async { autoreleasepool{
                                 QChatService.sync()
                             }}
