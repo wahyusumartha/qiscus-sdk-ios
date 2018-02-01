007c44a08cd8df4c89b3952eefcf0fedfae4e417
Ahmad Athaullah <ahmad.athaullah@gmail.com>
2018-02-01 11:12:46 +0700

    change version to 2.8.1

--- Qiscus/Qiscus/Qiscus.swift
+++ Qiscus/Qiscus/Qiscus.swift
@@ -24,7 +24,7 @@ var QiscusDBThread = DispatchQueue(label: "com.qiscus.db", attributes: .concurre
     
     static let sharedInstance = Qiscus()
     
-    static let qiscusVersionNumber:String = "2.8.0"
+    static let qiscusVersionNumber:String = "2.8.1"
     public static let client = QiscusClient.shared
     
     public static var showDebugPrint = false
