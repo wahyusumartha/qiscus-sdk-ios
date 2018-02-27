b350307719889dd0cbc65e849071ec90add28023
Ahmad Athaullah <ahmad.athaullah@gmail.com>
2018-02-27 13:48:54 +0700

    fix crash on post #1

--- Qiscus/Qiscus/Model/DataModel/QComment.swift
+++ Qiscus/Qiscus/Model/DataModel/QComment.swift
@@ -709,9 +709,11 @@ public class QComment:Object {
                 c.statusRaw = status.rawValue
             }
             if status == .deleted {
-                try! realm.write {
-                    c.text = "This message was deleted"
-                    c.typeRaw = QCommentType.text.name()
+                if !c.isInvalidated {
+                    try! realm.write {
+                        c.text = "This message was deleted"
+                        c.typeRaw = QCommentType.text.name()
+                    }
                 }
                 if let r = QRoom.threadSaveRoom(withId: rId){
                     if r.lastCommentUniqueId == uId {
