3d354edf5fa100c9768fe8b611c99df08a35fd31
amsibsam <amsibsam@gmail.com>
2018-06-05 15:30:21 +0700

    prepare for system cell

---
+++ QiscusUI/QiscusUI/View/ChatScene/ChatCell/QSystemCell.swift
@@ -0,0 +1,23 @@
+//
+//  QSystemCell.swift
+//  QiscusUI
+//
+//  Created by Rahardyan Bisma on 05/06/18.
+//
+
+import UIKit
+
+class QSystemCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
