f2ee1a3c64b95358a46378b80d821800b89afa4a
asharijuang <asharijuang@me.com>
2018-08-07 14:30:31 +0700

    room manager

---
+++ Qiscus/QRoom.swift
@@ -0,0 +1,18 @@
+//
+//  QRoom.swift
+//  Qiscus
+//
+//  Created by Qiscus on 07/08/18.
+//
+
+import Foundation
+import QiscusCore
+
+public class QRoomManager {
+    public static let shared = QRoomManager()
+    
+    public func loadAvatar() {
+        
+    }
+    
+}
